0bfcac7472dcfdcd96741e9264613da0ed1a891b
Carsten Meyke <carsten.meyke@gmail.com>
2025-05-18 10:53:48 +0200

    first working kde panel configuration

--- plasma.nix
+++ plasma.nix
@@ -1,34 +1,30 @@
 {
+
   programs.plasma = {
     enable = true;
   };
-  # programs.plasma.panels =
-  # [
-  #   {
-  #     iconTasks = {
-  #       launchers = [
-  #         "applications:org.kde.dolphin.desktop"
-  #         "applications:org.kde.konsole.desktop"
-  #       ];
-  #     };
-  #   }
-    # {
-    #   name = "org.kde.plasma.icontasks";
-    #   config = {
-    #     # system settings, dolphin, konsole, keepassxc, obsidian, remmina, steam, document scanner
-    #     General.launchers = [
-    #       "applications:org.kde.dolphin.desktop"
-    #       "applications:org.kde.konsole.desktop"
-    #     ];
-    #   };
-    #   position = {
-    #     horizontal = 0;
-    #     vertical = 0;
-    #   };
-    #   size = {
-    #     height = 0;
-    #     width = 0;
-    #   };
-    # }
-  # ];
+
+  programs.plasma.panels = [
+    {
+      widgets = [
+        "org.kde.plasma.kickoff"
+        "org.kde.plasma.pager"
+        {
+          iconTasks = {
+            # System Settings" ... "KeePassXC" "Obsidian" "Remmina" "Steam" "Document Scanner"
+            launchers = [
+              "applications:org.kde.dolphin.desktop"
+              "applications:firefox.desktop"
+              "applications:org.kde.konsole.desktop"
+              "applications:steam.desktop"
+            ];
+          };
+        }
+        "org.kde.plasma.marginsseparator"
+        "org.kde.plasma.systemtray"
+        "org.kde.plasma.digitalclock"
+        "org.kde.plasma.showdesktop"      ];
+    }
+  ];
+
 }
